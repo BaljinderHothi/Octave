bea5c519e855902b56b68580ae8a7d0e3073cddc
tareqbairouti <bosssk8er38@gmail.com>
2025-03-28 22:03:19 -0400

    Added a placeholder for  generateitinerary.tsx and randomitinerary.tsx

---
+++ pages/randomitinerary.tsx
@@ -0,0 +1,7 @@
+export default function RandomItinerary() {
+  return (
+    <div className="flex items-center justify-center min-h-screen text-xl font-semibold">
+      Will be implemented later
+    </div>
+  )
+}
